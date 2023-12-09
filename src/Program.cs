9e8e661aadb15eaf2e83bd56947fd6d2d1b9220f
iCyuba <48867623+iCyuba@users.noreply.github.com>
2023-12-09 19:57:27 +0100

    Implemented cron scheduling Uses quartz to do so...

--- src/Program.cs
+++ src/Program.cs
@@ -2,24 +2,16 @@ namespace BackupUtility
 {
     internal class Program
     {
-        static void Main()
+        static async Task Main()
         {
             // Load the backup jobs from the default config file
             var jobs = BackupJob.LoadFromConfig();
 
-            foreach (var job in jobs)
-            {
-                var id = Guid.NewGuid();
+            // Setup the backup scheduler
+            await BackupScheduler.Setup(jobs);
 
-                foreach (string target in job.Targets)
-                {
-                    // Create the backup
-                    var backup = new Handler(job, target);
-
-                    // Save the backup
-                    backup.Backup(id);
-                }
-            }
+            // Wait forever
+            await Task.Delay(-1);
         }
     }
 }
